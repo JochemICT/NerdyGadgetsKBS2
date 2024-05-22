8205f6bf89bed624d84c3ae84a02a7c831ca9ada
Jochem <s1201516@student.windesheim.nl>
2024-05-22 15:45:45 +0200

    view.php fix

--- view.php
+++ view.php
@@ -108,7 +108,7 @@ if($StockItem['IsChillerStock'] == true) { ?>
             </h2>
             <!-- formulier via POST en niet GET om te zorgen dat refresh van pagina niet het artikel onbedoeld toevoegt-->
             <form method="post">
-                <input type="number" name="stockItemID" value="<?php print($stockItemID) ?>" hidden>
+                <input type="number" name="stockItemID" value="<?php $stockItemID ?>" hidden>
                 <button type="submit" name="submit" value="Voeg toe aan winkelmandje">Voeg to aan winkelwagen</button>
             </form>
             <div class="QuantityText">
